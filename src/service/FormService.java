b589992dbf036a06e3fd16ad0bbc0841be4d3e79
Guilherme Lacerda <lacerdagui42@gmail.com>
2025-01-10 18:15:04 -0300

    Refatoração - Ajustes nos imports e nomes de classe

--- src/service/FormService.java
+++ src/service/FormService.java
@@ -5,7 +5,7 @@ import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
 
-public class Form {
+public class FormService {
     private static String PATH_QUESTIONS = "formulario.txt";
 
     protected static ArrayList<String> questions = new ArrayList<>();
