a49ec820a8376a973fa1095610a6c59c55bb8e26
Guilherme Lacerda <lacerdagui42@gmail.com>
2025-01-10 00:08:35 -0300

    Refatoramento: Desacomplando o caminho do .txt e o array de perguntas do método loadQuestions.

--- src/service/Form.java
+++ src/service/Form.java
@@ -6,24 +6,25 @@ import java.io.IOException;
 import java.util.ArrayList;
 
 public class Form {
-    public static void loadQuestions() {
-        String PATH_QUESTIONS = "formulario2.txt";
+    private static String PATH_QUESTIONS = "formulario.txt";
 
-        ArrayList<String> questions = new ArrayList<>();
+    protected static ArrayList<String> questions = new ArrayList<>();
+
+    public static void loadQuestions() {
 
         try (BufferedReader br = new BufferedReader(new FileReader(PATH_QUESTIONS))) {
             String line = br.readLine();
             while (line != null) {
                 questions.add(line);
-                line = br.readLine();
+                 line = br.readLine();
             }
         } catch (IOException e) {
             throw new RuntimeException("Error: " + e.getMessage());
         }
 
-        for (String question : questions) {
-            System.out.println(question);
-        }
-
     }
+
+
+
+
 }
